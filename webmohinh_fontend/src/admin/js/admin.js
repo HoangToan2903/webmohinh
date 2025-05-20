5813128359bb6fcba7616c9935dbb5534bc66466
DESKTOP-JC8STTK\admin <toanhd290803@gmail.com>
2025-05-20 17:28:12 +0700

    upload

---
+++ webmohinh_fontend/src/admin/js/admin.js
@@ -0,0 +1,9 @@
+function Admin() {
+  return (
+    <div >
+     <h1>Toi yeue em</h1>
+    </div>
+  );
+}
+
+export default Admin;
--- webmohinh_fontend/src/index.js
+++ webmohinh_fontend/src/index.js
@@ -1,13 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Admin from './admin/js/admin';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Router>
+      <Routes>
+        <Route path="/admin" element={<Admin />} />
+
+        </Routes>
+    </Router>
   </React.StrictMode>
 );
 

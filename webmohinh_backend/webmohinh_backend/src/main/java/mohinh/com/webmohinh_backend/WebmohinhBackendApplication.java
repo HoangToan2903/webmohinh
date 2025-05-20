60a4522a1db70b364db73745e93177905eb59fbd
DESKTOP-JC8STTK\admin <toanhd290803@gmail.com>
2025-05-20 12:19:51 +0700

    upload file

---
+++ webmohinh_backend/webmohinh_backend/src/main/java/mohinh/com/webmohinh_backend/WebmohinhBackendApplication.java
@@ -0,0 +1,13 @@
+package mohinh.com.webmohinh_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebmohinhBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebmohinhBackendApplication.class, args);
+	}
+
+}
---
+++ webmohinh_backend/webmohinh_backend/src/test/java/mohinh/com/webmohinh_backend/WebmohinhBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package mohinh.com.webmohinh_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebmohinhBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

edbf0dde03961b0746453b7060fa511bd52a85c9
DESKTOP-JC8STTK\admin <toanhd290803@gmail.com>
2025-06-09 17:49:36 +0700

    upload file

--- webmohinh_fontend/src/index.js
+++ webmohinh_fontend/src/index.js
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom/client';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter as Router, Route, Routes, Navigate } from "react-router-dom";
 import Admin from './admin/js/admin';
+import Home from './client/js/home'
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -10,14 +12,11 @@ root.render(
     <Router>
       <Routes>
         <Route path="/admin/:tab" element={<Admin />} />
-        {/* Redirect mặc định nếu chưa có tab */}
-        <Route path="/admin" element={<Navigate to="/admin/statistics" />} />
+        <Route path="/admin" element={<Navigate to="/admin/statistics"/>} />
+        <Route path="/home" element={<Home />} />
       </Routes>
     </Router>
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
